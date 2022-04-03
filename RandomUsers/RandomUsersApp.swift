65dba4e16f86acc2197f5b94fd9f54d4f589986b
Clement <clementkodra@gmail.com>
2022-04-02 22:27:41 -0400

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Clement Kodradjaja on 2022-04-02.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
